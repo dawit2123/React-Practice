2058b5964ca44322c4654407e635b3a7fa14dc13
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-17 21:57:21 +0300

    time counter app is built using react

---
+++ counter-challenge/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function App() {
+  return <Counter />;
+}
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const handleDecreaseStep = () => {
+    if (step > 1) setStep((step) => step - 1);
+  };
+  const handleIncreaseStep = () => setStep((step) => step + 1);
+  const handleIncreaseCount = () => setCount((count) => count + step);
+  const handleDecreaseCount = () => setCount((count) => count - step);
+  let dateCount = count;
+  let message;
+  let estimatedDate = new Date();
+  estimatedDate.setDate(estimatedDate.getDate() + dateCount);
+  if (dateCount === 0) message = `Today is ${estimatedDate.toDateString()}`;
+  else if (dateCount > 0)
+    message = `${dateCount} days from today is ${estimatedDate.toDateString()}`;
+  else if (dateCount < 0)
+    message = `${dateCount * -1} days ago was ${estimatedDate.toDateString()}`;
+  return (
+    <div className="container">
+      <div className="step">
+        <button className="decStep" onClick={handleDecreaseStep}>
+          -
+        </button>
+        <span>Step: {step}</span>
+        <button className="incStep" onClick={handleIncreaseStep}>
+          +
+        </button>
+      </div>
+      <div className="count">
+        <button className="decCount" onClick={handleDecreaseCount}>
+          -
+        </button>
+        <span>Count: {count}</span>
+        <button className="incCount" onClick={handleIncreaseCount}>
+          +
+        </button>
+      </div>
+      <div className="result">
+        <span>{message}</span>
+      </div>
+    </div>
+  );
+}
