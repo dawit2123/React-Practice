34172360f9220e75f71c1065f7f752d70aa82d83
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-17 16:00:37 +0300

    closing button doen

--- steps/src/App.js
+++ steps/src/App.js
@@ -5,38 +5,46 @@ const messages = [
   "Invest your new income 🤑",
 ];
 export default function App() {
-  let [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
+  const [isOpened, setIsOpened] = useState(true);
   const handlePrevious = () => {
     if (step > 1) setStep(step - 1);
   };
   const handleNext = () => {
     if (step < 3) setStep(step + 1);
   };
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <div className="message">
-        Step {step}: {messages[step - 1]}
-      </div>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-        {/* 7950f2 */}
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpened(!isOpened)}>
+        &times;
+      </button>
+      {isOpened && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <div className="message">
+            Step {step}: {messages[step - 1]}
+          </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
