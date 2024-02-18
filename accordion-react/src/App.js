3fbc7186c979590a155167191a435fc27bb42900
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-18 16:08:37 +0300

    second version of the acordion complete

--- accordion-react/src/App.js
+++ accordion-react/src/App.js
@@ -9,52 +9,50 @@ const items = [
     name: "What is React",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt explicabo fuga quas expedita totam! Quia distinctio enim cum voluptas mollitia optio, eius ipsam consequatur consectetur dolore, necessitatibus, eligendi adipisci.",
-    isOpened: true,
     id: Date.now(),
   },
   {
     name: "What is the best programming language to learn",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt explicabo fuga quas expedita totam! Quia distinctio enim cum voluptas mollitia optio, eius ipsam consequatur consectetur dolore, necessitatibus, eligendi adipisci.",
-    isOpened: false,
-    id: Date.now(),
+    id: Date.now() + 1,
   },
   {
     name: "How to love problem and get more problems",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt explicabo fuga quas expedita totam! Quia distinctio enim cum voluptas mollitia optio, eius ipsam consequatur consectetur dolore, necessitatibus, eligendi adipisci.",
-    isOpened: false,
-    id: Date.now(),
+    id: Date.now() + 2,
   },
 ];
 {
   /* <h2 className="faq">Frequently Asked Questions FAQ</h2>; */
 }
 function Accordion() {
-  const [isOpened, setIsOpened] = useState([false]);
+  return items.map((item, index) => (
+    <AccordionItem item={item} index={index} key={item.id} />
+  ));
+}
+function AccordionItem({ item, index }) {
+  const [isOpened, setIsOpened] = useState(false);
   function handleIsOpened(item) {
-    item.isOpened = !isOpened;
+    console.log("high");
     setIsOpened(!isOpened);
   }
-  return items.map((item, index) => (
+  return (
     <div className="container">
-      <div
-        className={`container-item ${item.isOpened === true ? "active" : ""}`}
-      >
+      <div className={`container-item ${isOpened === true ? "active" : ""}`}>
         <div className="parent-header" onClick={() => handleIsOpened(item)}>
           <span className="number">
             {index < 10 ? `0${index + 1}` : `${index + 1}`}
           </span>
           <h2 className="child-header">{item.name}</h2>
-          <button className="button">
-            {item.isOpened === true ? "-" : "+"}
-          </button>
+          <button className="button">{isOpened === true ? "-" : "+"}</button>
         </div>
 
         <p className="container-paragraph">
-          {item.isOpened === true ? item.description : ""}
+          {isOpened === true ? item.description : ""}
         </p>
       </div>
     </div>
-  ));
+  );
 }
