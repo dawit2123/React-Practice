d77cb6ef30e030275d8d944f5b3871e4f57ee0cb
dawit2123 <dawitzewdu2123@gmail.com>
2024-06-04 09:51:15 +0300

    worldwise: fake password only for frontend updated

--- worldwise/src/pages/Login.jsx
+++ worldwise/src/pages/Login.jsx
@@ -8,7 +8,7 @@ import styles from "./Login.module.css";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [password, setPassword] = useState("qwerty998393khik");
 
   const { login, isAuthenticated } = useAuth();
   const navigate = useNavigate();
